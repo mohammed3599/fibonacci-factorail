7f834511d592e1b71c6abb5cfa24385593041c85
mohammed3599 <kataloni1999@gmail.com>
2023-06-08 13:55:32 +0400

    add calculate sum of digit

--- Program.cs
+++ Program.cs
@@ -24,9 +24,22 @@
             else
                 return n * calFactorial(n - 1);  
         }
+
+        static int calSumDigit(int num)
+        {
+            if (num < 10)  
+                return num;
+
+            int lastDigit = num % 10;  
+            int digits = num / 10;  
+
+            return lastDigit + calSumDigit(digits);  
+        }
+
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Please Enter a number  to calculate its factorial: ");
+            Console.WriteLine("Please Enter a number to calculate its factorial: ");
             int f = int.Parse(Console.ReadLine());
             System.Console.WriteLine($"The {f}! is: {calFactorial(f)}");
             Console.WriteLine();
@@ -34,6 +47,11 @@
             Console.WriteLine("Please Enter a number  to calculate its Fibonacci: ");
             int a = int.Parse(Console.ReadLine());
             System.Console.WriteLine($"The fibonacci of {a} is: {calFibonacci(a)}");
+            Console.WriteLine();
+
+            Console.WriteLine("Please Enter a number to calculate its digits: ");
+            int s = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The sum digits of {s} is: {calSumDigit(s)}");
         }
     }
 }
